6b97ec17241c31902ae97cf1d1ac4067a00e4290
Iamneo <techadmin@iamneo.ai>
2023-08-14 10:17:54 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,13 +1,27 @@
 package com.examly.springapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.persistence.*;
+import java.util.List;
 
+
+@RestController
 @SpringBootApplication
 public class SpringappApplication {
 
+	@PersistenceUnit
+    private EntityManagerFactory emf;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
+
 	}
+
+	
+
+
 
 }
